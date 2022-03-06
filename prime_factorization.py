795d7069321968a25a58c5fa9ee9d536c58fa94e
n_na <91656188+n-nana@users.noreply.github.com>
2022-03-07 00:47:09 +0900

    Create prime_factorization.py

---
+++ prime_factorization.py
@@ -0,0 +1,14 @@
+
+def fact(n):
+    ans = []
+    i = 2
+    while i*i <= N:
+        if n%i == 0:
+            ans.append(i)
+            n //= i
+        else:
+            i += 1
+    if n!= 1:
+        ans.append(n)
+    return ans
+
